a40c2c8cbb3d26551a6cbbaa27899352785986e5
Rain <emeraldsnorlax@disroot.org>
2024-02-18 14:57:59 +0000

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<h1>Welcome to SvelteKit, this is a barcode</h1>
+<a href="./foo">click here to go to foo</a>
+<span>if you inline styles in productions in svelte.config.js, the font will fail to load in production.</span>
+
+<style>
+  h1 {
+    color: red;
+    font-family: 'Libre Barcode 128 Text', sans-serif
+  }
+</style>
---
+++ src/routes/foo/+page.svelte
@@ -0,0 +1,9 @@
+<h1>Test, this should be rendered using Libre Barcode 128.</h1>
+<a href="/">back to root</a>
+
+<style>
+  h1 {
+    color: blue;
+    font-family: 'Libre Barcode 128 Text', sans-serif
+  }
+</style>
